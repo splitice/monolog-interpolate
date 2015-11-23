4228d7fadf9ae2a1c98823fefe4c8c622de97bda
splitice <mat999@gmail.com>
2015-11-23 11:24:36 +1100

    monolog interpolation processor

---
+++ src/Splitice/Logging/InterpolateProcessor.php
@@ -0,0 +1,35 @@
+<?php
+namespace Splitice\Logging;
+
+
+class InterpolateProcessor
+{
+	/**
+	 * Interpolate
+	 *
+	 * From PSR-3 doc
+	 *
+	 * @param $message
+	 * @param array $context
+	 * @return string
+	 */
+	protected function interpolate($message, array $context = array())
+	{
+		$from = $to = array();
+
+		foreach ($context as $key => $val) {
+			if (is_string($val)) {
+				$from[] = '{' . $key . '}';
+				$to[] = $val;
+			}
+		}
+
+		return strtr($message, $from, $to);
+	}
+
+	function __invoke(array $record)
+	{
+		$record['message'] = $this->interpolate($record['message'], $record);
+		return $record;
+	}
+}
